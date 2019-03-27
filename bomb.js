a0569b1098af5c0c91882e3505454c0bc0451d2e
= <Ben.coding2018@gmail.com>
2019-03-28 00:23:05 +0800

    number is correct

--- bomb.js
+++ bomb.js
@@ -8,10 +8,21 @@ class Bomb{
         this.revealed = false;
     }
 
+    set_bomb(){
+        this.bomb = true;
+    }
+
+    is_bomb(){
+        return this.bomb;
+    }
+
+    set_number(num){
+        this.number = num;
+    }
+
     show(){
         if(!this.revealed){
             noFill();
-            
             rect(this.i*40, this.j*40, 40, 40);
         }
         else if(this.revealed && this.bomb){
@@ -24,8 +35,10 @@ class Bomb{
             fill(150, 150, 150);
             rect(this.i*40, this.j*40, 40, 40);
             fill(0, 0, 0);
-            textSize(25);
-            text(this.number, this.i*40+14, this.j*40+28);
+            if(this.number != 0){
+                textSize(25);
+                text(this.number, this.i*40+14, this.j*40+28);
+            }
         }
     }
 
--- game.js
+++ game.js
@@ -11,6 +11,32 @@ class Game{
                 this.grid[i][j] = new Bomb(i, j);
             }
         }
+
+        for(let i=0;i<10;i++){
+            let choice_x = Math.floor((Math.random() * 10));
+            let choice_y = Math.floor((Math.random() * 10));
+            this.grid[choice_x][choice_y].set_bomb();
+        }
+
+
+        // set number
+        for(let i=0;i<w;i++){
+            for(let j=0;j<h;j++){
+                let num = 0;
+                for(let x_in=-1;x_in<=1;x_in++){
+                    for(let y_in=-1;y_in<=1;y_in++){
+                        if((x_in == 0 && y_in == 0) || i+x_in<0 || j+y_in<0 || i+x_in>w-1 || j+y_in>h-1){
+                            continue;
+                        }
+                        else if(this.grid[i+x_in][j+y_in].is_bomb()){
+                            num++;
+                        }
+                    }
+                }
+                this.grid[i][j].set_number(num);
+            
+            }
+        }
     }
     
     show(){
